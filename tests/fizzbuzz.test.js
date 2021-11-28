d74a6b2c37755649b6fd4ce9f876153ecf71c682
Matthew Kiggen <28730291+mattkiggen@users.noreply.github.com>
2021-11-28 13:53:54 +0200

    initial commit

---
+++ tests/fizzbuzz.test.js
@@ -0,0 +1,29 @@
+const { fizzBuzz } = require('../fizzbuzz');
+
+describe('fizzBuzz', () => {
+  it('should throw an error if input is not a Number', () => {
+    expect(() => {
+      fizzBuzz('5');
+    }).toThrow();
+  });
+
+  it('should return FizzBuzz if input is divisible by 3 and 5', () => {
+    const result = fizzBuzz(15);
+    expect(result).toBe('FizzBuzz');
+  });
+
+  it('should return "Fizz" if input is only divisible by 3', () => {
+    const result = fizzBuzz(3);
+    expect(result).toBe('Fizz');
+  });
+
+  it('should return "Buzz" if input is only divisible by 5', () => {
+    const result = fizzBuzz(5);
+    expect(result).toBe('Buzz');
+  });
+
+  it('should return the input back if it meets none of the conditions', () => {
+    const result = fizzBuzz(1);
+    expect(result).toBe(1);
+  });
+});
